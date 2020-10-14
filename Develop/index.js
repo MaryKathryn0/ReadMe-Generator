b1073539837aae2a5b543d92bb9a44f24188acc4
Mary Kathryn <msmarykathrynholt@gmail.com>
2020-10-14 17:37:24 -0500

    changed the anchor links to reverse them and see if that works

--- Develop/index.js
+++ Develop/index.js
@@ -59,20 +59,27 @@ function generateMarkdown(response) {
 # ${response.Title}
 
 # Table of Contents
-  -Description
-  -Installation
-  -Usage
-  -License
-  -Contributors
-  -Test
-  -Questions
-# [Description](#description)${response.Description}
-# [Installation(#installation)${response.Installation}
-# [Usage](#usage)${response.Usage}
-# [License](#license)${response.License}
-# [Contributors](#contributors)${response.Contributors}
-# [Test](#test)${response.Test}
-# [Questions](#questions)${response.Questions}
+  -[Description](#description)
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributors](#contributors)
+  -[Test](#test)
+  -[Questions](#questions)
+# Description
+  -${response.Description}
+# Installation
+  -${response.Installation}
+# Usage
+  -${response.Usage}
+# License
+  -${response.License}
+# Contributors
+  -${response.Contributors}
+# Test
+-${response.Test}
+# Questions
+  -${response.Questions}
     `;
 }
 
--- Develop/readme.md
+++ Develop/readme.md
@@ -1,20 +1,27 @@
 
 
-# Cats
-# meow meow moew
+# Meow Meow Meow
 
 # Table of Contents
-  -Installation
-  -Usage
-  -License
-  -Contributors
-  -Test
-  -Questions
-
-# use youtube
-# to make me happy
-# youtube, git hub, brain
-# Halle
-# Just use node index.js and respond to questions
-# MaryKathryn0 msmarykathrynholt@gmail.com
+  -[Description](#description)
+  -[Installation(#installation)
+  -[Usage](#usage)
+  -[License](#license)
+  -[Contributors](#contributors)
+  -[Test](#test)
+  -[Questions](#questions)
+# Description
+  -A fun app for cats to enjoy
+# Installation
+  -First find the laser pointer. Second Turn it on. Installation Complete
+# Usage
+  -To entertain cats and make them do cute things like jump in the air and run crazily like wild animals. 
+# License
+  -youtube, my brain, class examples
+# Contributors
+  -my two cats - Roger Sterling and Lois Lane
+# Test
+-n/a
+# Questions
+  -MaryKathryn0 mmmm@gmail.com
     
